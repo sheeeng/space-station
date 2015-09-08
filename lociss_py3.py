cccbe18526417bfa996169ecf9eb39e238343f10
Leonard Lee <sheeeng@gmail.com>
2015-09-08 13:30:00 +0200

    Fix TypeError.
    Fix `TypeError: the JSON object must be str, not 'bytes'` error.[1]
    
    [1] http://stackoverflow.com/a/32250010/4763512

--- lociss_py3.py
+++ lociss_py3.py
@@ -6,7 +6,7 @@ req = urllib.request.Request("http://api.open-notify.org/iss-now.json")
 
 def locateiss():
     response = urllib.request.urlopen(req)
-    obj = json.loads(response.read())
+    obj = json.loads(response.read().decode('utf8'))
     print("Time : %s | Lat./Long. : %.16f,%.16f" % (obj['timestamp'], obj['iss_position']['latitude'], obj['iss_position']['longitude']))
     print("Time :", obj['timestamp'], "|", "Lat./Long. :", obj['iss_position']['latitude'], ",", obj['iss_position']['longitude'])
 
