84bbe4b8a6ef0d6aaaa724530adbcc04d9b792ea
Leonard Lee <sheeeng@gmail.com>
2015-09-08 13:25:00 +0200

    Add Python 3 version.
    Convert the script to Python 3 environment, using 2to3.py tool.

---
+++ lociss_py3.py
@@ -0,0 +1,15 @@
+import json
+import time
+import urllib.request, urllib.error, urllib.parse
+
+req = urllib.request.Request("http://api.open-notify.org/iss-now.json")
+
+def locateiss():
+    response = urllib.request.urlopen(req)
+    obj = json.loads(response.read())
+    print("Time : %s | Lat./Long. : %.16f,%.16f" % (obj['timestamp'], obj['iss_position']['latitude'], obj['iss_position']['longitude']))
+    print("Time :", obj['timestamp'], "|", "Lat./Long. :", obj['iss_position']['latitude'], ",", obj['iss_position']['longitude'])
+
+while True:
+    locateiss()
+    time.sleep(5)
